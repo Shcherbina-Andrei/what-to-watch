e08546c9dfd2cde276cdeeb8e61d6e4edded88ff
andrei110295@yandex.ru <andrei110295@yandex.ru>
2023-01-10 18:37:52 +0300

    Создал компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
+import MainPage from '../../pages/main-page';
+
+const film = {
+  title: 'Sicario',
+  genre: 'thriller',
+  year: 2015
+};
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <MainPage title={film.title} genre={film.genre} year={film.year}/>
+  );
 }
 
 export default App;
